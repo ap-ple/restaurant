595230a20671edd5d28751b95015a46c23b4dc76
Johan Durish <jdurish2@gmail.com>
2025-03-11 10:00:17 -0400

    Add info paragraph

--- src/homeContent.js
+++ src/homeContent.js
@@ -2,4 +2,7 @@
 const welcome = document.createElement("h2");
 welcome.innerText = "Welcome!";
 
-export default [welcome];
+const information = document.createElement("p");
+information.innerText = "This is a restaurant where you can order food.";
+
+export default [welcome, information];
