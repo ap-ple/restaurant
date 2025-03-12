c6793c0a1c4d9d10c4e849feefa968d2b9e1808e
Johan Durish <jdurish2@gmail.com>
2025-03-12 09:28:59 -0400

    Add reservations number

--- src/contactContent.js
+++ src/contactContent.js
@@ -2,4 +2,7 @@
 const heading = document.createElement("h2");
 heading.innerText = "Contact Us";
 
-export default [heading,];
+const reservations = document.createElement("p");
+reservations.innerText = "For reservations, call 800-555-0100";
+
+export default [heading, reservations,];
