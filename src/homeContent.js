050e9a9dea665d6ea052d4c4afa7a4b59e1de5a9
Johan Durish <jdurish2@gmail.com>
2025-03-12 09:17:27 -0400

    Remove paragraph from home page

--- src/homeContent.js
+++ src/homeContent.js
@@ -5,7 +5,4 @@ heading.innerText = "Welcome!";
 const information = document.createElement("p");
 information.innerText = "This is a restaurant where you can order food.";
 
-const orderingInformation = document.createElement("p");
-orderingInformation.innerText = "Click the Menu button above to start your order.";
-
-export default [heading, information, orderingInformation,];
+export default [heading, information,];
