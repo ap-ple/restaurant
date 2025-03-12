dd0b7c6c852e715a6df43327939b83ee2061b6f9
Johan Durish <jdurish2@gmail.com>
2025-03-12 09:33:41 -0400

    Add period to paragraph

--- src/contactContent.js
+++ src/contactContent.js
@@ -3,6 +3,6 @@ const heading = document.createElement("h2");
 heading.innerText = "Contact Us";
 
 const reservations = document.createElement("p");
-reservations.innerText = "For reservations, call 800-555-0100";
+reservations.innerText = "For reservations, call 800-555-0100.";
 
 export default [heading, reservations,];
